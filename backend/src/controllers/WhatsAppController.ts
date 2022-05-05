fd7bdef588a0c60cffc378b21310acb56d1997a1
owenzap <103150476+owenzap@users.noreply.github.com>
2022-05-05 04:03:21 -0300

    Update WhatsAppController.ts

--- backend/src/controllers/WhatsAppController.ts
+++ backend/src/controllers/WhatsAppController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { getIO } from "../libs/socket";
 import { removeWbot } from "../libs/wbot";
 import { StartWhatsAppSession } from "../services/WbotServices/StartWhatsAppSession";
-
+import AppError from “../errors/AppError”;
 import CreateWhatsAppService from "../services/WhatsappService/CreateWhatsAppService";
 import DeleteWhatsAppService from "../services/WhatsappService/DeleteWhatsAppService";
 import ListWhatsAppsService from "../services/WhatsappService/ListWhatsAppsService";
@@ -26,6 +26,11 @@ export const index = async (req: Request, res: Response): Promise<Response> => {
 };
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
+  const WhatsApps = await ListWhatsAppsService();
+  
+  if (WhatsApps.length >= Number(process.env.CONNECTIONS_LIMIT)) {
+    throw new AppError("ERR_NO_PERMISSION", 403);
+  }
   const {
     name,
     status,
