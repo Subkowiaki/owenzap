37e4e8c21f0f507bd48851611b3d2cdb975ae3aa
owenzap <103150476+owenzap@users.noreply.github.com>
2022-05-30 17:19:32 -0300

    Fechamento automático do ticket enviado por meio do endpoint api

--- backend/src/controllers/ApiController.ts
+++ backend/src/controllers/ApiController.ts
@@ -13,10 +13,11 @@ import CheckContactNumber from "../services/WbotServices/CheckNumber";
 import GetProfilePicUrl from "../services/WbotServices/GetProfilePicUrl";
 import SendWhatsAppMedia from "../services/WbotServices/SendWhatsAppMedia";
 import SendWhatsAppMessage from "../services/WbotServices/SendWhatsAppMessage";
+import UpdateTicketService from "../services/TicketServices/UpdateTicketService";
 
 type WhatsappData = {
   whatsappId: number;
-};
+}
 
 type MessageData = {
   body: string;
@@ -36,11 +37,10 @@ const createContact = async (
   await CheckIsValidContact(newContact);
 
   const validNumber: any = await CheckContactNumber(newContact);
-  const profilePicUrl = await GetProfilePicUrl(
-    validNumber.jid.replace(/\D/g, "")
-  );
 
-  const number = validNumber.jid.replace(/\D/g, "");
+  const profilePicUrl = await GetProfilePicUrl(validNumber);
+
+  const number = validNumber;
 
   const contactData = {
     name: `${number}`,
@@ -62,7 +62,7 @@ const createContact = async (
       throw new AppError(`whatsapp #${whatsappId} not found`);
     }
   }
-
+  
   const createTicket = await FindOrCreateTicketService({
     whatsappId: whatsapp.id,
     contact,
@@ -109,5 +109,11 @@ export const index = async (req: Request, res: Response): Promise<Response> => {
     await SendWhatsAppMessage({ body, ticket: contactAndTicket, quotedMsg });
   }
 
-  return res.send();
+  setTimeout(async () => {
+    await UpdateTicketService({
+      ticketId: contactAndTicket.id,
+      ticketData: { status: "closed" }
+    });
+  }, 1000);
+  return res.send({ error: "SUCCESS" });
 };
