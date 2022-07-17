f33d39e72a50c744538e77ad759b5b999b8526df
Marcelo Junior <skowiaki@gmail.com>
2022-07-17 17:31:45 -0300

    Revert "Configuracao de banco de dados"
    This reverts commit 7f1cb541b412a9885b17d7481e1aa4453c9a1216.

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -7,9 +7,9 @@ module.exports = {
   },
   dialect: process.env.DB_DIALECT || "mysql",
   timezone: "-03:00",
-  host: "multichat.ce9l05qknrdw.sa-east-1.rds.amazonaws.com", // process.env.DB_HOST,
-  database: "markey", // process.env.DB_NAME,
-  username: "markey", // process.env.DB_USER,
-  password: "Msjks1705$", // process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   logging: false
 };
