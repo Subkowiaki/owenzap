50ec5479e77c0f0292b6ede578ae915f22fe66c9
Marcelo Junior <skowiaki@gmail.com>
2022-07-17 21:01:20 -0300

    Revert "Revert "Revert "Inclussao do relatorio"""
    This reverts commit 33b5ba5403b8bc75a1cb06ff82f6355287345d38.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -34,7 +34,6 @@ const Routes = () => {
             <LoggedInLayout>
               <Route exact path="/" component={HomeDashBoard} isPrivate />
               <Route exact path="/payments" component={Payments} isPrivate />
-              <Route exact path="/store-report" component={Payments} isPrivate />
               <Route
                 exact
                 path="/tickets/:ticketId?"
